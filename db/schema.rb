099286bb814ed34192b5a595ddbff3e247acba82
unknown <ke@risedurning-dx.ds.corp.yahoo.com>
2012-01-18 13:14:09 -0800

    ajax

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120114005645) do
+ActiveRecord::Schema.define(:version => 20120117214232) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
